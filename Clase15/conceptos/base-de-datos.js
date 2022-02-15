e22c7041a90b3f46d2d11f0d473ffed2fc998e42
Mariano Jimenez Perez <marianojimenezperez1@gmail.com>
2022-02-14 22:14:25 -0300

    SQL conceptos

---
+++ Clase15/conceptos/base-de-datos.js
@@ -0,0 +1,37 @@
+/*
+
+¿Qué es una base de datos?
+
+La base de datos es un repositorio persistente que nos permite almacenar gran número de información de una forma organizada para su futura consulta,
+realización de búsquedas, nuevo ingreso de datos y muchas otras operaciones. 
+Un servidor de base de datos es un contenedor que puede alojar un gran número de bases de datos y ofrece los servicios para conectarlas a los clientes.
+Mediante los clientes podemos interactuar con las bases de datos y estos pueden estar implementados en modo consola, en modo aplicación gráfica o desde nuestra aplicación de servidor.
+
+Clientes de base de datos
+
+La arquitectura cliente-servidor es un modelo de diseño de software en el que las tareas se reparten entre los proveedores de recursos o servicios, llamados servidores, y los demandantes, llamados clientes
+Un cliente realiza peticiones a otro programa, el servidor, quien le da respuesta.
+Un cliente de base de datos se conecta e interactúa con el servidor de base de datos.
+
+Tipos de clientes de base de datos
+
+-Cliente CLI (Command Line Interface): Es un cliente que interactúa con la base de datos mediante el uso de una consola.(ej: MYSQL COMMAND LINE CLIENT)
+-Cliente GUI (graphical user interface): Es un cliente que interactúa con la base de datos mediante el uso de una aplicación gráfica.(ej: MySQL Workbench)
+-Cliente Web : Es un cliente que interactúa con la base de datos a través de una página web mediante el uso de un navegador. (ej: phpmyadmin)
+-Cliente de aplicación:  Es un cliente que está implementado dentro de nuestra aplicación de backend y sirve para que nuestro programa se conecte e interactúe con la base de datos. (ej: módulo "mongoose" de node)
+
+¿Qué es SQL?
+
+La sigla que se conoce como SQL corresponde a la expresión inglesa Structured Query Language (en español “Lenguaje de Consulta Estructurado”)
+SQL es un tipo de lenguaje vinculado con la gestión de bases de datos de carácter relacional, que permite la especificación de distintas clases de operaciones entre éstas. 
+Gracias a la utilización del álgebra y de cálculos relacionales, el SQL brinda la posibilidad de realizar consultas con el objetivo de recuperar información de las bases de datos de manera sencilla.
+
+MySQL y MariaDB
+
+MySQL es un sistema de gestión de bases de datos relacional desarrollado bajo licencia dual: Licencia pública general/Licencia comercial por Oracle Corporation y está considerada como la base de datos de código abierto más popular del mundo.
+
+MariaDB es un sistema de gestión de bases de datos derivado de MySQL con licencia GPL (General Public License).
+
+MySQL y MariaDB son compatibles entre sí a nivel funcional.
+
+*/
